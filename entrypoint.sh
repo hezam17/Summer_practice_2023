4f3a9ebf10a0dd5d6951eda33c6e1a0534716f5c
Raheeb <Raheeb772652540@gmail.ccom>
2023-08-27 01:12:24 +0300

    added new code for GitHub actions

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,10 +3,6 @@ set -eu
 
 export GITHUB="true"
 
-# Debugging: Print TELEGRAM_TOKEN and TELEGRAM_CHAT_ID
-echo "DEBUG: TELEGRAM_TOKEN=$TELEGRAM_TOKEN"
-echo "DEBUG: TELEGRAM_CHAT_ID=$TELEGRAM_CHAT_ID"
-
 [ -n "$*" ] && export TELEGRAM_MESSAGE="$*"
 
 /bin/drone-telegram
